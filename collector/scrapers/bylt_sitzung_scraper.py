661bb5a2a638dac8a4fa6e8ac1806320c89e660f
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-04-03 14:27:14 +0200

    sample data and improvements

--- collector/scrapers/bylt_sitzung_scraper.py
+++ collector/scrapers/bylt_sitzung_scraper.py
@@ -26,8 +26,9 @@ class BYLTSitzungScraper(Scraper):
     def __init__(self, config, session: aiohttp.ClientSession):
         start_date = datetime.datetime.now().astimezone(datetime.UTC) - datetime.timedelta(days=1)
         listing_urls = []
-        for day in range(4*7):
-            listing_urls.append(start_date + datetime.timedelta(days=day))
+        for week in range(4): # four weeks
+            date = (start_date + datetime.timedelta(weeks=week)).date()
+            listing_urls.append(f"https://www.bayern.landtag.de/ajaxcalendar.html?week={date.day}.{date.month}.{date.year}&currentDate=1.{date.month}.{date.year}&all=true&fullYear=false&contentUid=51169")
 
         super().__init__(config, uuid.uuid4(), listing_urls, session)
         # Add headers for API key authentication
@@ -39,7 +40,15 @@ class BYLTSitzungScraper(Scraper):
         async with self.session.get(url) as result:
             object = json.loads(await result.text())
             listing_soup = BeautifulSoup(object["html"], "html.parser")
-            items = listing_soup.find_all("div", class_="agenda-item")
+            listitems = listing_soup.find_all("li")
+            current_date = None
+            for li in listitems:
+                if li.get("role") == "heading":
+                    # this is a heading, usually a date
+                    current_date = li.text
+                elif li.find("div", class_="agenda-item"):
+                    
+
             resitem = []
             for item in items:
                 if "Ausschuss " in item.text or "Plenarsitzung" in item.text:
