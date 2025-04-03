4e7c5437b9fed213a7912640f9ec84bb529ae1d5
Benedikt Schäfer <benediktschaefer@protonmail.com>
2025-04-03 16:49:13 +0200

    date-wise slots in listing extractor and item extraction starting point

--- collector/scrapers/bylt_sitzung_scraper.py
+++ collector/scrapers/bylt_sitzung_scraper.py
@@ -41,20 +41,65 @@ class BYLTSitzungScraper(Scraper):
             object = json.loads(await result.text())
             listing_soup = BeautifulSoup(object["html"], "html.parser")
             listitems = listing_soup.find_all("li")
+
+            day_items = {}
             current_date = None
             for li in listitems:
                 if li.get("role") == "heading":
                     # this is a heading, usually a date
-                    current_date = li.text
-                elif li.find("div", class_="agenda-item"):
-                    
-
-            resitem = []
-            for item in items:
-                if "Ausschuss " in item.text or "Plenarsitzung" in item.text:
-                    resitem.append(item)
-            return [resitem] # an item is a list of individual sessions
+                    current_date = parse_natural_date(li.text)
+                    day_items[current_date] = []
+                elif li.find("div", class_="agenda-item") != None:
+                    agitem = li.find("div", class_="agenda-item")
+                    # this is an actual entry with a date
+                    title = agitem.find("p", class_="h4").text
+                    if title.startswith("Ausschuss für") or title.startswith("Plenarsitzung"):
+                        day_items[current_date].append(agitem)
+                else:
+                    continue
+            
+            return day_items.items() # an item is a list of individual sessions ordered by day
     
     async def item_extractor(self, listing_item):
-        for sitzung in listing_item:
-            pass
+        # a listing item is a pair of (date, [entries])
+        for (termin, sitzungen) in listing_item:
+            for sitzung in sitzungen:
+                time = sitzung.find("div", class_="date").text.split(":")
+
+                full_termin = datetime.datetime(year=termin.year, month=termin.month,day=termin.day,
+                                                hour=int(time[0]), minute=int(time[1]))
+                title_line = sitzung.find("p", class_="h4").text
+                title = None
+                grname = None
+                if " - " in title_line:
+                    split = title_line.split("-")
+                    title = split[1].strip()
+                    grname = split[0].strip()
+                else:
+                    grname = title_line
+                gremium  = models.Gremium.from_dict({
+                    "parlament": "BY",
+                    "wahlperiode": 19,
+                    "name": grname
+                })
+                sitzung = models.Sitzung.from_dict({
+                    "titel": title,
+                    "termin": full_termin,
+                    "gremium": gremium,
+                    "nummer" : None,
+                    "public" : True,
+                    "link": None,
+                    "tops": [],
+                    "dokumente": [],
+                    "experten": [] if "Anhörung" in title_line else None,
+                })
+                dok_span = sitzung.find("span", class_="agenda-docs")
+                for link in dok_span.find_all("a"):
+                    doc_link = link.get("href")
+                    # for each doc build + extract document
+
+
+def parse_natural_date(date) -> datetime.date:
+    split = date.split(" ")
+    number = split[0][:-1]
+    month = split[1].lower()
